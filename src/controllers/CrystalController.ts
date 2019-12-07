fd915287f1eca270a21bea426a976f29f729e4e1
akinsola <thomas.lawanson@gmail.com>
2019-12-07 14:02:45 +0000

    updated crystal controller

--- src/controllers/CrystalController.ts
+++ src/controllers/CrystalController.ts
@@ -3,6 +3,7 @@ import { ResponseService } from '../services/ResponseService';
 import { Crystal } from '../models/Crystal';
 import { DatabaseService } from '../services/DatabaseService';
 import validator from 'validator';
+import { User } from '../models/User';
 
 export const addCrystal: APIGatewayProxyHandler = async (event, context) => {
     try {
@@ -29,5 +30,107 @@ export const addCrystal: APIGatewayProxyHandler = async (event, context) => {
         return ResponseService.success(crystal);
     } catch (error) {
         return ResponseService.error(error.message, error.statusCode);
+    }
+}
+
+export const getCrystalsForUser: APIGatewayProxyHandler = async (event, context) => {
+    try {
+        let db: DatabaseService;
+        let crystals;
+        // Retrieve user id
+        let user_id = event.pathParameters.user_id;
+
+        // Validate user id
+        if(!user_id) {
+            throw new Error('Request is missing user_id');
+        }
+        if(!validator.isUUID(user_id, 4)) {
+            throw new Error('User_Id must be a UUID');
+        }
+
+        db = new DatabaseService('crystals');
+        await db.scan({
+            sender_id: user_id,
+            receiver_id: user_id
+        }, false).then((resp) => {
+            crystals = resp.Items;
+        });
+
+        return ResponseService.success(crystals);
+    } catch (error) {
+        return ResponseService.error(error.message, error.statusCode);        
+    }
+
+}
+
+export const sendCrystalToUser: APIGatewayProxyHandler = async (event, context) => {
+    try {
+        let db: DatabaseService;
+        // Receive crystal id and receiver id
+        const body = JSON.parse(event.body);
+
+        // Validate Crystal Id
+        if(!body.crystal_id) {
+            throw new Error('Request is missing crystal_id');
+        }
+
+        if(!validator.isUUID(body.crystal_id, 4)) {
+            throw new Error('crystal_id must be a UUID');
+        }
+        // Validate Receiver Id
+
+        if(!body.receiver_id) {
+            throw new Error('Request is missing receiver_id');
+        }
+
+        if(!validator.isUUID(body.receiver_id, 4)) {
+            throw new Error('receiver_id must be a UUID');
+        }
+
+        // Get crystal from db
+        db = new DatabaseService('crystals');
+        let crystal: Crystal;
+        await db.getItem(body.crystal_id).then((resp) => {
+            crystal = resp.Item;
+        });
+        
+        // Check if crystal exists
+        if(!crystal) {
+            throw new Error('Crystal does not exist')
+        }
+
+        //Get Receiver User from db
+        let receiverUser: User;
+        db.updateTableName('users');
+        await db.getItem(body.receiver_id).then((resp) => {
+            receiverUser = resp.Item;
+        });
+
+        // Get Send User from db
+        let senderUser: User;
+        await db.getItem(crystal.sender_id).then((resp) => {
+            senderUser = resp.Item;
+        })
+        
+        // Update crystal
+        console.log(typeof crystal);
+        crystal.receiver_id = receiverUser.id;
+        crystal.sent = true;
+        // crystal.sendTo(receiverUser.id);
+
+        // Update connections for both users
+        receiverUser.no_of_connections++;
+        senderUser.no_of_connections++;
+
+        await db.putItem(receiverUser);
+        await db.putItem(senderUser);
+
+        db.updateTableName('crystals');
+        await db.putItem(crystal);
+
+        return ResponseService.success(crystal);
+
+    } catch (error) {
+        return ResponseService.error(error.message, error.statusCode);                
     }
 }
